e1b2ba32ab1e35177e0f2e105f2d01ba75050a93
tianxie812 <xietian628@yahoo.com>
2020-09-19 15:06:30 -0700

    added rest framework add on

--- blogging/views.py
+++ blogging/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
-from blogging.models import Post
+from blogging.models import Post,Category
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from blogging.serializers import UserSerializer, GroupSerializer,PostSerializer,CategorySerializer
 
 
 def detail_view(request, post_id):
@@ -19,3 +23,37 @@ def list_view(request):
     posts = published.order_by('-published_date')
     context = {'posts': posts}
     return render(request, 'blogging/list.html', context)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Post to be viewed or edited.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CatagoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Catagory to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
